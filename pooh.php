8a345d33008c4bf55ad19cb4577b498d3f58de68
jjean_tw <ccumouse@gmail.com>
2019-02-25 16:34:33 +0800

    clear commit

---
+++ pooh.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Winnie the pooh</title>
+    <link rel="stylesheet" href="css/pooh.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Croissant+One|Lora|Pacifico" rel="stylesheet">
+</head>
+<body>
+<div class="mine-navbar-container flex-container-row">
+    <div class="mine-navbar-title">Winnie the Pooh</div>
+    <div class="mine-navbar-stretch"></div>
+    <div class="mine-navbar-item">Home</div>
+    <div class="mine-navbar-item">Profile</div>
+    <div class="mine-navbar-item">More</div>
+</div>
+
+<header class="header-container flex-container-row">
+    <div class="header-text">A Short Introduction to <br>Winnie the Pooh</div>
+</header>
+
+
+<img id="profile" src="images/profile.jpg" alt="profile">
+
+<div class="article">
+    <div class="heading">Pooh's Appearance</div>
+    Pooh is an anthropomorphic teddy bear. He is short, has yellow fur, and a round belly. He has black dot eyes, a
+    black nose, a short snout, and small round ears. There is also stitching on his belly and bottom, which occasionally
+    rips open. He only wears a small red short-sleeved shirt, in which his belly hangs out of.
+</div>
+
+<div class="fixed-img" id="img-1"></div>
+
+
+<div class="article">
+    <div class="heading">Personality</div>
+    Pooh Bear resides in the Hundred Acre Wood, an imaginative world inhabited by anthropomorphic stuffed toys and
+    animals that Christopher Robin frequents. Pooh is stuffed with fluff, and therefor impervious to pain. Due to his
+    obesity, his seams burst open on occasion, but Pooh has become so accustomed to this that he can tie it back
+    together in seconds. Pooh's tree house—which is under the name Sanders—is mostly filled with jars and jars of honey,
+    which he harvests from bee hives throughout the day.
+</div>
+
+<div class="fixed-img" id="img-2"></div>
+
+
+<div class="article">
+    <div class="heading">Origin</div>
+    "Winnie-the-Pooh," as he was originally called, first appeared in a story written by A.A. Milne, who based the
+    characters off his young owner Christopher Robin Milne and the boy's stuffed animals. The original toy bear was
+    named after Winnie (short for "Winnipeg"), a bear that had been adopted as a cub by a Canadian soldier during World
+    War I, who was a popular attraction at the London Zoo. The stories were popular in the Disney household, motivating
+    Walt to start production on a series of featurettes starring the honey-obsessed bear and his collection of friends.
+</div>
+
+<div class="fixed-img" id="img-3"></div>
+
+
+<div class="article">
+    <div class="heading">Legacy</div>
+    <ul>
+        <li>One of the best known characters in British children's literature, a 2011 poll saw Winnie the Pooh voted
+            onto the list of icons of England.
+        </li>
+        <li>Forbes magazine ranked Pooh the most valuable fictional character in 2002, with merchandising products alone
+            generating more than $5.9 billion that year.
+        </li>
+        <li>In 2005, Pooh generated $6 billion, a figure surpassed by only Mickey Mouse.</li>
+        <li>In 2006, Pooh received a star on the Hollywood Walk of Fame, marking the 80th birthday of Milne's
+            creation.
+        </li>
+        <li>The bear is such a popular character in Poland that a Warsaw street is named for him, Ulica Kubusia
+            Puchatka. There is also a street named after him in Budapest (Micimackó utca).
+        </li>
+    </ul>
+</div>
+
+<div class="fixed-img" id="img-4"></div>
+
+<div class="thanks-for-watching flex-container-column">
+    <img class="profile-in-thanks" src="images/cute-profile.jpg" alt="cute-profile.jpg">
+    <div>Thank you for watching</div>
+</div>
+
+<div class="fixed-img flex-container-row" id="img-5">
+    The End
+</div>
+
+<footer>
+    <div class="flex-container-column copyright">
+        <div>
+            This page is written for learning purposes.<br>
+            If you are the owner of resource used in the page, you can contact me for removing your resource.<br><br>
+        </div>
+        <div id="contact-background">
+            <div id="contact-container">
+                <span>©2019 designed by Mao,Yin-Nian CCU-CSIE 405410010. All rights reserved.</span>
+                <span>Email: yt8956789@csie.io</span>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
+
